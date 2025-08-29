f0181672a03443eea7b728fb2538a67925acf71f
MatheusMohr <matheusmohr000@gmail.com>
2025-08-29 19:17:07 -0300

    add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home-component/home-component';
+import { ProdutoComponent } from './component/produto-component/produto-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },          
+  { path: 'produtos', component: ProdutoComponent }    
+];
